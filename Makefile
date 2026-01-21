c01fdbd173b4e387c35114bf329c8dfdddd41069
unlomtrois <vadim.kuz02@gmail.com>
2026-01-21 10:11:21 +0300

    refactor: move logic to pomo package

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: build test
 
 build:
-	go build -o pomo cmd/pomo/main.go
+	go build -o bin/pomo cmd/pomo/main.go
 
 test:
 	go test ./...
---
+++ internal/pomo/pomodoro.go
@@ -0,0 +1,119 @@
+package pomo
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func formatDuration(d time.Duration) string {
+	h := int(d.Hours())
+	m := int(d.Minutes()) % 60
+	return fmt.Sprintf("%02d:%02d", h, m)
+}
+
+type Pomodoro struct { // This thing is saved to csv / database / toggl integration
+	Title     string        `csv:"title"`
+	Message   string        `csv:"message"`
+	StartTime time.Time     `csv:"start_time"`
+	StopTime  time.Time     `csv:"stop_time"`
+	Duration  time.Duration `csv:"duration"`
+}
+
+func NewPomodoro(title string, message string, startTime time.Time, stopTime time.Time, duration time.Duration) *Pomodoro {
+	return &Pomodoro{
+		Title:     title,
+		Message:   message,
+		StartTime: startTime,
+		StopTime:  stopTime,
+		Duration:  duration,
+	}
+}
+
+func (p *Pomodoro) Strings() []string {
+	startTime := p.StartTime.Format(time.DateTime)
+	stopTime := p.StopTime.Format(time.DateTime)
+	duration := formatDuration(p.Duration)
+	return []string{p.Title, startTime, stopTime, duration}
+}
+
+func (p *Pomodoro) Notify() error {
+	notifyCmd := fmt.Sprintf("DISPLAY=:0 notify-send -u critical '%s' '%s'", p.Title, p.Message)
+	atTime := fmt.Sprintf("now + %d minutes", int(p.Duration.Minutes()))
+	atCmd := exec.Command("at", atTime)
+
+	// Pipe the notify-send command to at via stdin
+	stdin, err := atCmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("Error creating stdin pipe: %v\n", err)
+	}
+
+	if err := atCmd.Start(); err != nil {
+		return fmt.Errorf("Error starting at command: %v\n", err)
+	}
+
+	_, err = stdin.Write([]byte(notifyCmd + "\n"))
+	if err != nil {
+		return fmt.Errorf("Error writing to stdin: %v\n", err)
+	}
+	if err := stdin.Close(); err != nil {
+		return fmt.Errorf("Error closing stdin: %v\n", err)
+	}
+
+	if err := atCmd.Wait(); err != nil {
+		return fmt.Errorf("Error: %v\nMake sure 'at' daemon (atd) is running.\n", err)
+	}
+
+	fmt.Printf("   You'll be notified at %s\n", p.StopTime.Format("15:04:05"))
+	return nil
+}
+
+func InitCsv(filename string) error {
+	if _, err := os.Stat(filename); os.IsExist(err) {
+		return nil
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Error creating pomodoro.csv: %v\n", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	if err := writer.Write([]string{"title", "start_time", "stop_time", "duration"}); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	fmt.Println("Pomodoro csv initialized")
+
+	return nil
+}
+
+func (p *Pomodoro) Save(filename string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return fmt.Errorf("Error opening pomodoro.csv: %v\n", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	if err := writer.Write(p.Strings()); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	fmt.Println("Pomodoro added to csv")
+
+	return nil
+}
