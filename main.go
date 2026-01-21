8b31928ec04a705e65764af8de38b865e66d39a4
unlomtrois <vadim.kuz02@gmail.com>
2026-01-21 09:04:15 +0300

    improve api

--- main.go
+++ main.go
@@ -17,19 +17,59 @@ func formatDuration(d time.Duration) string {
 }
 
 type Pomodoro struct { // This thing is saved to csv / database / toggl integration
-	Title     string `csv:"title"`
-	StartTime string `csv:"start_time"`
-	StopTime  string `csv:"stop_time"`
-	Duration  string `csv:"duration"`
+	Title     string        `csv:"title"`
+	Message   string        `csv:"message"`
+	StartTime time.Time     `csv:"start_time"`
+	StopTime  time.Time     `csv:"stop_time"`
+	Duration  time.Duration `csv:"duration"`
 }
 
-func NewPomodoro(title string, startTime time.Time, stopTime time.Time, duration time.Duration) *Pomodoro {
+func NewPomodoro(title string, message string, startTime time.Time, stopTime time.Time, duration time.Duration) *Pomodoro {
 	return &Pomodoro{
 		Title:     title,
-		StartTime: startTime.Format(time.DateTime),
-		StopTime:  stopTime.Format(time.DateTime),
-		Duration:  formatDuration(duration),
+		Message:   message,
+		StartTime: startTime,
+		StopTime:  stopTime,
+		Duration:  duration,
 	}
+}
+
+func (p *Pomodoro) Strings() []string {
+	startTime := p.StartTime.Format(time.DateTime)
+	stopTime := p.StopTime.Format(time.DateTime)
+	duration := formatDuration(p.Duration)
+	return []string{p.Title, startTime, stopTime, duration}
+}
+
+func (p *Pomodoro) Notify() error {
+	notifyCmd := fmt.Sprintf("DISPLAY=:0 notify-send -u critical '%s' '%s'", p.Title, p.Message)
+	atTime := fmt.Sprintf("now + %d minutes", p.Duration)
+	atCmd := exec.Command("at", atTime)
+
+	// Pipe the notify-send command to at via stdin
+	stdin, err := atCmd.StdinPipe()
+	if err != nil {
+		return fmt.Errorf("Error creating stdin pipe: %v\n", err)
+	}
+
+	if err := atCmd.Start(); err != nil {
+		return fmt.Errorf("Error starting at command: %v\n", err)
+	}
+
+	_, err = stdin.Write([]byte(notifyCmd + "\n"))
+	if err != nil {
+		return fmt.Errorf("Error writing to stdin: %v\n", err)
+	}
+	if err := stdin.Close(); err != nil {
+		return fmt.Errorf("Error closing stdin: %v\n", err)
+	}
+
+	if err := atCmd.Wait(); err != nil {
+		return fmt.Errorf("Error: %v\nMake sure 'at' daemon (atd) is running.\n", err)
+	}
+
+	fmt.Printf("   You'll be notified at %s\n", p.StopTime.Format("15:04:05"))
+	return nil
 }
 
 func main() {
@@ -40,16 +80,6 @@ func main() {
 	var title string
 	var message string
 	var noNotify bool
-
-	newCmd.IntVar(&duration, "d", 25, "Duration in minutes (default: 25)")
-	newCmd.StringVar(&title, "t", "Pomodoro Timer", "Notification title")
-	newCmd.StringVar(&message, "m", "Pomodoro finished! Time for a break.", "Notification message")
-	newCmd.BoolVar(&noNotify, "n", false, "Don't notify")
-
-	restCmd.IntVar(&duration, "d", 5, "Duration in minutes (default: 5)")
-	restCmd.StringVar(&title, "t", "Break Timer", "Notification title")
-	restCmd.StringVar(&message, "m", "Break finished! Time for a pomodoro.", "Notification message")
-	restCmd.BoolVar(&noNotify, "n", false, "Don't notify")
 
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage: pomodoro <command> [options]")
@@ -70,8 +100,16 @@ func main() {
 
 	switch os.Args[1] {
 	case "new":
+		newCmd.IntVar(&duration, "d", 25, "Duration in minutes (default: 25)")
+		newCmd.StringVar(&title, "t", "Pomodoro Timer", "Notification title")
+		newCmd.StringVar(&message, "m", "Pomodoro finished! Time for a break.", "Notification message")
+		newCmd.BoolVar(&noNotify, "n", false, "Don't notify")
 		newCmd.Parse(os.Args[2:])
 	case "rest":
+		restCmd.IntVar(&duration, "d", 5, "Duration in minutes (default: 5)")
+		restCmd.StringVar(&title, "t", "Break Timer", "Notification title")
+		restCmd.StringVar(&message, "m", "Break finished! Time for a pomodoro.", "Notification message")
+		restCmd.BoolVar(&noNotify, "n", false, "Don't notify")
 		restCmd.Parse(os.Args[2:])
 	default:
 		flag.Usage()
@@ -92,11 +130,8 @@ func main() {
 	// Escape single quotes in title and message
 	safeTitle := strings.ReplaceAll(title, "'", "'\"'\"'")
 	safeMessage := strings.ReplaceAll(message, "'", "'\"'\"'")
-	notifyCmd := fmt.Sprintf("DISPLAY=:0 notify-send -u critical '%s' '%s'", safeTitle, safeMessage)
 
-	// pomodoro
-
-	pomodoro := NewPomodoro(safeTitle, startTime, stopTime, time.Duration(duration)*time.Minute)
+	pomodoro := NewPomodoro(safeTitle, safeMessage, startTime, stopTime, time.Duration(duration)*time.Minute)
 
 	file, err := os.OpenFile("pomodoro.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
@@ -107,7 +142,7 @@ func main() {
 
 	writer := csv.NewWriter(file)
 
-	marchalledPomodoro := []string{pomodoro.Title, pomodoro.StartTime, pomodoro.StopTime, pomodoro.Duration}
+	marchalledPomodoro := pomodoro.Strings()
 	if err := writer.Write(marchalledPomodoro); err != nil {
 		fmt.Fprintf(os.Stderr, "Error writing to pomodoro.csv: %v\n", err)
 		os.Exit(1)
@@ -128,33 +163,8 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Create the at command with "now + X minutes"
-	atTime := fmt.Sprintf("now + %d minutes", duration)
-	atCmd := exec.Command("at", atTime)
-
-	// Pipe the notify-send command to at via stdin
-	stdin, err := atCmd.StdinPipe()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating stdin pipe: %v\n", err)
+	if err := pomodoro.Notify(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error notifying: %v\n", err)
 		os.Exit(1)
 	}
-
-	if err := atCmd.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting at command: %v\n", err)
-		os.Exit(1)
-	}
-
-	_, err = stdin.Write([]byte(notifyCmd + "\n"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing to stdin: %v\n", err)
-		os.Exit(1)
-	}
-	stdin.Close()
-
-	if err := atCmd.Wait(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\nMake sure 'at' daemon (atd) is running.\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("   You'll be notified at %s\n", stopTime.Format("15:04:05"))
 }
