56cf728cb1efb587d4471b158184630cd3dc2b9b
unlomtrois <vadim.kuz02@gmail.com>
2026-01-22 11:59:47 +0300

    refactor(pomo): move initCsv to sep file

---
+++ internal/pomo/csv.go
@@ -0,0 +1,33 @@
+package pomo
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func InitCsv(filename string) error {
+	if _, err := os.Stat(filename); os.IsExist(err) {
+		return nil
+	}
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("Error creating pomodoro.csv: %v\n", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	if err := writer.Write([]string{"title", "start_time", "stop_time", "duration"}); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		return fmt.Errorf("Error writing to pomodoro.csv: %v\n", err)
+	}
+
+	fmt.Println("Pomodoro csv initialized")
+
+	return nil
+}
