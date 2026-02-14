3502145152c6e1ac72ad206fb11d83984482a138
unlomtrois <vadim.kuz02@gmail.com>
2026-02-14 10:55:29 +0300

    move notify logic into notifier

--- internal/pomo/pomodoro.go
+++ internal/pomo/pomodoro.go
@@ -4,7 +4,6 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
-	"os/exec"
 
 	"strings"
 	"time"
@@ -54,70 +53,22 @@ func (p *Pomodoro) Strings() []string {
 	return []string{p.Title, startTime, stopTime, duration}
 }
 
-func (p *Pomodoro) notifyViaAt(hint string) error {
-	atTime := fmt.Sprintf("now + %d minutes", int(p.Duration.Minutes()))
-	cmd := exec.Command("at", atTime)
-
-	// Pipe the notify-send command to at via stdin
-	notifyCmd := fmt.Sprintf("pomo notify -t %q -m %q --hint=%q", p.Title, p.Message, hint)
-	sleepAndNotify := fmt.Sprintf("sleep %d && %s", p.StopTime.Second(), notifyCmd)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return fmt.Errorf("Error creating stdin pipe: %v\n", err)
-	}
-	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("Error starting at command: %v\n", err)
-	}
-	if _, err = stdin.Write([]byte(sleepAndNotify + "\n")); err != nil {
-		return fmt.Errorf("Error writing to stdin: %v\n", err)
-	}
-	if err := stdin.Close(); err != nil {
-		return fmt.Errorf("Error closing stdin: %v\n", err)
-	}
-	if err := cmd.Wait(); err != nil {
-		return fmt.Errorf("Error: %v\nMake sure 'at' daemon (atd) is running.\n", err)
-	}
-
-	fmt.Printf("You'll be notified at %s\n", p.StopTime.Format("15:04:05"))
-	return nil
-}
-
-func (p *Pomodoro) notifyViaSystemd(hint string) error {
-	delay := max(time.Until(p.StopTime), 0)
-	delayStr := fmt.Sprintf("%.3fs", delay.Seconds())
-	unitName := fmt.Sprintf("pomo-notify-%d", time.Now().UnixNano())
-
-	pomoPath, err := exec.LookPath("pomo")
-	if err != nil {
-		return fmt.Errorf("could not find pomo executable: %v", err)
-	}
-
-	cmd := exec.Command("systemd-run",
-		"--user", "--collect", "--unit="+unitName, "--on-active="+delayStr, "--timer-property=AccuracySec=100ms",
-		pomoPath, "notify", "-t="+p.Title, "-m="+p.Message, "--hint="+hint,
-	)
-
-	if out, err := cmd.CombinedOutput(); err != nil {
-		return fmt.Errorf("Failed to schedule notification: %v: %s", err, out)
-	}
-
-	fmt.Printf("You'll be notified at %s (via systemd timer %s)\n", p.StopTime.Format("15:04:05"), unitName)
-	return nil
-}
-
 func (p *Pomodoro) NotifyLater(hint string) error {
+	var notifier Notifier
+
 	if utils.HasSystemd() {
-		return p.notifyViaSystemd(hint)
+		notifier = &SystemdNotifier{}
+	} else if utils.HasAt() {
+		notifier = &AtNotifier{}
+	} else {
+		return fmt.Errorf("neither 'systemd-run' nor 'at' found. Please install one of them for background notifications")
 	}
 
-	if utils.HasAt() {
-		if p.Duration < 1*time.Minute {
-			fmt.Println("Warning: \"at\" does not support sub-minute precision")
-		}
-		return p.notifyViaAt(hint)
+	if err := notifier.NotifyLater(p, hint); err != nil {
+		return err
 	}
 
-	return fmt.Errorf("neither 'systemd-run' nor 'at' found. Please install one of them for background notifications")
+	return nil
 }
 
 func (p *Pomodoro) Save(filename string) error {
