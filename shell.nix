db1e003f729b21a263597cc1de9c7d73752ddbb1
unlomtrois <vadim.kuz02@gmail.com>
2026-02-21 16:49:19 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    packages = with pkgs; [ go_1_26 libgcc gnumake libnotify ];
+    packages = with pkgs; [ go_1_26 gcc libnotify ];
 
     shellHook = ''
       echo "Dev environment loaded!"
